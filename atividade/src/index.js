c4c82136fcc046f2f6fd27f1be54f89f6fd6f81e
felipesaugusto <ifelipexdd@gmail.com>
2023-04-25 20:09:08 -0300

    Primeiro commit

--- atividade/src/index.js
+++ atividade/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Login from './Login/Login'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Teste</h1>
+    <h1>Tela de Login</h1>
+    <Login />
   </React.StrictMode>
 );
