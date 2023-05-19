08593dc6faa770041ad2de36e72ec6c8c8bbe790
felipesaugusto <ifelipexdd@gmail.com>
2023-05-18 23:49:31 -0300

    test1

--- atividade/src/index.js
+++ atividade/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Perfil from './Perfil/Perfil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Teste</h1>
+    <Perfil></Perfil>
   </React.StrictMode>
 );
