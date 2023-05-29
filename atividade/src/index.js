4d79e0dd66dde79efca77f6b369b3fd39d866d1c
matheuspolesca <matheuspolesca@gmail.com>
2023-05-29 15:31:32 -0300

    Página de produto realizada anos atrás

--- atividade/src/index.js
+++ atividade/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Exemplo from './Exemplos/Exemplo de produto';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Teste</h1>
+    <div className='Main'>
+      <Exemplo />
+    </div>
   </React.StrictMode>
 );
