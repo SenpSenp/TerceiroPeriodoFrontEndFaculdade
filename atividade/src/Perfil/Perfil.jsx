853dea76219044616d165f3d80e8ad5199b48a6a
felipesaugusto <ifelipexdd@gmail.com>
2023-06-02 18:11:59 -0300

    final

--- atividade/src/Perfil/Perfil.jsx
+++ atividade/src/Perfil/Perfil.jsx
@@ -1,71 +1,63 @@
-import "./Perfil.css"
 import React from "react";
-import foto from "./foto/fotoPerfil.png"
+import foto from "./foto/fotoPerfil.png";
+import "./Perfil.css"
 
 const Perfil = () => {
-
   return (
-    <div className="container-fluid" style={{ marginTop: "20px" }}>
+    <div className="container-fluid mt-3">
       <div className="row">
-        <div className="col-md-4" style={{ height: "200px" }}>
+        <div className="col-md-4">
           <div className="card" style={{ height: "200px", marginBottom: "40px" }}>
             <div className="card-body">
-            <img src={foto} alt="Imagem" style={{ width: "90px" }} />
-            <p style={{ fontSize: "18px", fontWeight: "bold" }}>
-              Bem-vindo, (nome do usuário)
-            </p>
+              <img src={foto} alt="Imagem" style={{ width: "90px" }} />
+              <p style={{ fontSize: "18px", fontWeight: "bold" }}>Bem-vindo, (nome do usuário)</p>
             </div>
           </div>
         </div>
-            <div className="col-md-12" style={{ textAlign: "left", marginTop: "20px", fontSize: "18px", fontWeight: "bold" }}>
-          <p className="text1">RESUMO DO SEU ÚLTIMO PEDIDO</p>
-            </div>
-
-
-        <div className="col-md-6" style={{ height: "400px" }}>
+        <div className="col-md-12">
+          <p className="text1 mt-3">RESUMO DO SEU ÚLTIMO PEDIDO</p>
+        </div>
+        <div className="col-md-6">
           <div className="card" style={{ height: "350px", marginBottom: "40px" }}>
-            <div className="card-body" style={{ marginTop: "30px", marginLeft: "50px" }}>
-              
-                <span className="btn btn-primary">NUMERO PEDIDO</span>
-                <span className="btn btn-primary" style={{marginLeft: "125px" }}>STATUS PEDIDO</span>
-                <span className="btn btn-primary" style={{marginLeft: "160px" }}>DATA</span>
-                <span className="btn btn-primary" style={{marginLeft: "150px" }}>PAGAMENTO</span>
-            </div>
-
-
-            <div className="card-body-2" style={{ marginTop: "20px", marginLeft: "90px", marginBottom: "30px"  }}>
-              
-                <span className="btn btn-primary">#1231231231</span>
-                <span className="btn btn-primary">Concluido</span>
-                <span className="btn btn-primary">20/03/2012</span>
-                <span className="btn btn-primary">PIX</span>
-            </div>
-
+          <div className="card-body mt-3 ml-5">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>NUMERO PEDIDO</th>
+                <th>STATUS PEDIDO</th>
+                <th>DATA</th>
+                <th>PAGAMENTO</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>#1231231231</td>
+                <td>ConcluÍdo</td>
+                <td>20/03/2012</td>
+                <td>PIX</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
             <hr />
-
-            <div className="card-body-2" style={{ marginTop: "10px", marginLeft: "90px", marginBottom: "50px"  }}>
-                <span className="btn btn-primary">ENDEREÇO</span>
-                <span className="btn btn-primary" style={{marginLeft: "500px" }}>AZUL</span> 
+            <div className="card-body mt-2 ml-5 mb-5">
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <th>ENDEREÇO</th>
+                    <th>CÓDIGO DE RASTREIO</th>
+                  </tr>
+                  <tr>
+                    <td>Rua Martins Barbosa</td>
+                    <td>12A659FBM23</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
-            <div className="card-body-2" style={{ marginTop: "10px", marginLeft: "70px", marginBottom: "20px"  }}>
-                <span className="btn btn-primary">Rua Martins Barbosa</span>
-                <span className="btn btn-primary" style={{marginLeft: "500px" }}>AZUL</span> 
-            </div>
-
             <hr />
-
-            <div className="card-body-2" style={{ marginTop: "40px", marginLeft: "90px"  }}>
-              
-                <span className="btn btn-primary">#1231231231</span>
-                <span className="btn btn-primary">Concluido</span>
-                
-            </div>
-
-
+            <button className="btn-text">VER TODOS OS PEDIDOS</button>
           </div>
         </div>
-
-
         <div className="col-md-4">
           <div className="card">
             <div className="card-body">
