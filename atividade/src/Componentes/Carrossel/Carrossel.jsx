487156cd9daddbe84651dba76f6ee3c861152031
SenpSenp <lucas3.14ccinini@gmail.com>
2023-06-23 17:17:43 -0300

    Minor change in carrossel Adicionadas imagens melhores Adicionada função para passar as imagens sozinho

--- atividade/src/Componentes/Carrossel/Carrossel.jsx
+++ atividade/src/Componentes/Carrossel/Carrossel.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Carrossel.css';
 import img1 from './img/imagem1.png';
 import img2 from './img/imagem2.png';
 import img3 from './img/imagem3.png';
+import img4 from './img/imagem4.png';
+import img5 from './img/imagem5.png';
+import img6 from './img/imagem6.png';
 
 export const Carrossel = () => {
-  const imagens = [img1, img2, img3];
+  const imagens = [img1, img2, img3, img4, img5, img6];
   const [indiceImagemAtual, setIndiceImagemAtual] = useState(0);
 
   const handleProximaImagem = () => {
@@ -19,6 +22,13 @@ export const Carrossel = () => {
       indiceAnterior === 0 ? imagens.length - 1 : indiceAnterior - 1
     );
   };
+
+  useEffect(() => {
+    const interval = setInterval(handleProximaImagem, 3500); 
+    return () => {
+      clearInterval(interval);
+    };
+  });
 
   return (
     <div className="carrossel">
