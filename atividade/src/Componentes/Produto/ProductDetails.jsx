f7e712d71213d6d6dac490f428d54a41e8b89f92
matheuspolesca <matheuspolesca@gmail.com>
2023-06-25 17:07:17 -0300

    Editado o ProductDetails

--- atividade/src/Componentes/Produto/ProductDetails.jsx
+++ atividade/src/Componentes/Produto/ProductDetails.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import { Row, Col, Image, Form, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import './ProductDetails.css';
 
 function ProductDetails({ product }) {
+  const history = useHistory();
+  
   const [quantity, setQuantity] = useState(1);
 
+    // will take you to a detail page with a id of product
+    function getProductDetails(product) {
+      return history.push(`/api/products/${product.id}`);
+    }
   function handleQuantityChange(e) {
     setQuantity(e.target.value);
   }
@@ -12,12 +19,18 @@ function ProductDetails({ product }) {
   function handleAddToCart() {
     // Add the product and the selected quantity to the cart
   }
+    
 
   return (
     <div>
       <Row className="product-details">
       <Col xs={12} md={6} className="product-image-container">
-        <Image src={product.image} fluid className="product-image" />
+      <Image
+        src={product.image}
+        fluid
+        className="product-image"
+        
+      />
       </Col>
       <Col xs={12} md={6}>
         <h1 className="product-name">{product.name}</h1>
