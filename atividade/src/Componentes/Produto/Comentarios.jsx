156b030985779641e820a1194d6ab109b39da6b0
matheuspolesca <matheuspolesca@gmail.com>
2023-06-26 16:57:16 -0300

    Comentar não leva mais para a home

--- atividade/src/Componentes/Produto/Comentarios.jsx
+++ atividade/src/Componentes/Produto/Comentarios.jsx
@@ -1,33 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Row, Col } from 'react-bootstrap';
 
-//Comentários já realizados
-
-
-const Comentar = ({idProduto}) => {
-
-    const [comentarios, setComentarios] = useState([]);
+const Comentar = ({ idProduto }) => {
+  const [comentarios, setComentarios] = useState([]);
 
   useEffect(() => {
-    const fetchComentarios = async () => {
-      try {
-        const response = await fetch(`http://localhost:4000/comentarios?idProduto=${idProduto}`);
-        const data = await response.json();
-        setComentarios(data);
-      } catch (error) {
-        console.error('Erro ao obter os comentarios:', error);
-      }
-    };
-
     fetchComentarios();
   }, []);
 
-
-  const history = useHistory();
+  const fetchComentarios = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/comentarios?idProduto=${idProduto}`);
+      const data = await response.json();
+      setComentarios(data);
+    } catch (error) {
+      console.error('Erro ao obter os comentarios:', error);
+    }
+  };
 
   const [formData, setFormData] = useState({
     id: '',
@@ -44,25 +36,20 @@ const Comentar = ({idProduto}) => {
     }));
   };
 
-
   const handleCadastro = async () => {
     try {
-
       const response = await axios.get('http://localhost:4000/comentarios');
       const comentarios = response.data;
 
       const maxId = comentarios.reduce((max, comentario) => (comentario.id > max ? comentario.id : max), 0);
       const nextId = maxId + 1;
 
-
       const novoComentario = {
         id: nextId,
         nome: formData.nome,
         comentario: formData.comentario,
-        idProduto:  idProduto
-        
+        idProduto: idProduto
       };
-
 
       const comentarPromise = new Promise((resolve) =>
         setTimeout(() => {
@@ -84,67 +71,61 @@ const Comentar = ({idProduto}) => {
         comentario: ''
       });
 
-      history.push('/Login');
+      fetchComentarios();
     } catch (error) {
       console.error('Erro ao cadastrar:', error.message);
-        toast.error('Erro no cadastro. Verifique os dados e tente novamente.')
-      }
+      toast.error('Erro no cadastro. Verifique os dados e tente novamente.');
+    }
   };
 
   return (
     <div>
-        <div className='sessaoComentarios'>
-        
+      <div className='sessaoComentarios'>
         <Row className="product-description-row">
-        <Col xs={12}>
-        <h1>
-            Comentários do produto:
-        </h1>
-
-            {comentarios.map((comentarios) => (
-                <div>
-                    <h2> {comentarios.nome} disse:</h2>
-                    <p className="product-description"> {comentarios.comentario}</p>
-                </div>
+          <Col xs={12}>
+            <h1>Comentários do produto:</h1>
+            {comentarios.map((comentario) => (
+              <div key={comentario.id}>
+                <h2>{comentario.nome} disse:</h2>
+                <p className="product-description">{comentario.comentario}</p>
+              </div>
             ))}
-        </Col>
-        
-      </Row>
-        </div>
-        <div className="ComentarioContainer">
-      <h2 className="Comentario-h2">Caixa de comentário:</h2>
-      <form className="ComentarioForms">
-        <br />
-        <label className="Comentario-label">
-          Nome Público:
-          <input
-            type="text"
-            name="nome"
-            value={formData.nome}
-            onChange={handleChange}
-            className="Comentario-input"
-          />
-        </label>
-        <br />
-        <label className="Comentario-label">
-          Comentário:
-          <input
-            type="text"
-            name="comentario"
-            value={formData.comentario}
-            onChange={handleChange}
-            className="Comentario-input"
-          />
-        </label>
-        <br />
-        <button type="button" onClick={handleCadastro} className="Comentario-button">
-          Enviar comentário
-        </button>
-      </form>
-      <ToastContainer />
+          </Col>
+        </Row>
+      </div>
+      <div className="ComentarioContainer">
+        <h2 className="Comentario-h2">Caixa de comentário:</h2>
+        <form className="ComentarioForms">
+          <br />
+          <label className="Comentario-label">
+            Nome Público:
+            <input
+              type="text"
+              name="nome"
+              value={formData.nome}
+              onChange={handleChange}
+              className="Comentario-input"
+            />
+          </label>
+          <br />
+          <label className="Comentario-label">
+            Comentário:
+            <input
+              type="text"
+              name="comentario"
+              value={formData.comentario}
+              onChange={handleChange}
+              className="Comentario-input"
+            />
+          </label>
+          <br />
+          <button type="button" onClick={handleCadastro} className="Comentario-button">
+            Enviar comentário
+          </button>
+        </form>
+        <ToastContainer />
+      </div>
     </div>
-    </div>
-    
   );
 };
 
